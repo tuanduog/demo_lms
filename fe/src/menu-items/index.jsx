b1bf826a78391894c594b44067e00153417955c1
tuanduog <tuanduog04@gmail.com>
2025-09-19 09:37:43 +0700

    fe

---
+++ fe/src/menu-items/index.jsx
@@ -0,0 +1,12 @@
+// project-imports
+import pages from './pages';
+import samplePage from './sample-page';
+import support from './support';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+  items: [samplePage, pages, support]
+};
+
+export default menuItems;
